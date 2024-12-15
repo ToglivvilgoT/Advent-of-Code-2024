5cdb3ff5da3ca55ebb94951c9cff2f4c1c8b4cb8
ToglivvilgoT <vilgot.johansen@hotmail.com>
2024-12-15 12:21:46 +0100

    finished (finally!!!) day 13 part 2

--- day13.py
+++ day13.py
@@ -1,11 +1,19 @@
-from aoc import get_input, get_input_lines, uncurry, in_bounds, get_neighbours
-from functools import partial, reduce
+"""Solution for day 13"""
+from aoc import get_input
 from itertools import product
-from math import gcd
 import numpy
 
 
-def format_input(inp: str):
+X = int
+Y = int
+Abutton = list[X | Y]
+Bbutton = list[X | Y]
+Prize = list[X | Y]
+Machine = tuple[Abutton, Bbutton, Prize]
+
+
+def format_input(inp: str) -> list[Machine]:
+    """Formats the input into a list of Machines."""
     machines = []
     for machine in inp.split('\n\n'):
         lines = machine.splitlines()
@@ -28,20 +36,26 @@ def format_input(inp: str):
     return machines
 
 
-def tup_add(tup1, tup2):
-    return [tup1[0] + tup2[0], tup1[1] + tup2[1]]
+def lst_add(lst1: list[int], lst2: list[int]) -> list[int]:
+    """Adds the element togheter from two lists, lists must have length 2."""
+    return [lst1[0] + lst2[0], lst1[1] + lst2[1]]
 
 
-def tup_mul(tup, fctr):
-    return list(map(lambda x: x * fctr, tup))
+def lst_mul(lst: list[int], fctr: int) -> list[int]:
+    """Multiplies each element of lst by fctr."""
+    return list(map(lambda x: x * fctr, lst))
 
 
-def is_solution(a, b, machine):
+def is_solution(a: int, b: int, machine: Machine):
+    """Returns True if a and b amount of Abutton and Bbutton presses gets the prize."""
     a_button, b_button, expected = machine
-    return tup_add(tup_mul(a_button, a), tup_mul(b_button, b)) == expected
+    return lst_add(lst_mul(a_button, a), lst_mul(b_button, b)) == expected
 
 
-def solve(machines):
+def solve(machines: list[Machine]) -> int:
+    """Solves part 1 and returns the answer.
+    It does this by just brute force trying all combinations up to 100 presses for each button.
+    """
     result = 0
     for machine in machines:
         solutions = set()
@@ -54,6 +68,10 @@ def solve(machines):
 
 
 def eu_solve(a, b):
+    """First part of solving a diofantic equation.
+    Did not end up using this function since it did not provide an answer
+    it just gives an infinit list of possible answers.
+    """
     if a == b:
         return ((1, 1), (0, 1))
     elif a % b == 1:
@@ -65,7 +83,8 @@ def eu_solve(a, b):
     return next_b, next_a
 
 
-def eucledes(machine):
+def solveOne(machine: Machine) -> int:
+    """Solve one machine, return the answer for that machine or 0 if prize cant be grabed."""
     (ax, ay), (bx, by), (x, y) = machine
     equations = numpy.array([
         [ax, bx],
@@ -74,25 +93,31 @@ def eucledes(machine):
     solution = numpy.array([x, y])
     result = numpy.linalg.solve(equations, solution).astype(int)
     
-    test1 = equations[0][0] * result[0] + equations[0][1] * result[1]
-    test2 = equations[1][0] * result[0] + equations[1][1] * result[1]
-    if test1 == x and test2 == y:
-        return result[0] * 3 + result[1]
-    else:
-        print(test1 - x, test2 - y)
-        return 0
+    # I use the for loop to check the values around result
+    # Since numpy.linalg.solve uses floats the answer may experience some rounding error
+    # there for this for loop is needed.
+    for dx, dy in product(range(-10, 11), repeat=2):
+        res_x = result[0] + dx
+        res_y = result[1] + dy
+        test_x = ax * res_x + bx * res_y
+        test_y = ay * res_x + by * res_y
+        if test_x == x and test_y == y:
+            return res_x * 3 + res_y
+    return 0
 
 
-def solve2(machines):
+def solve2(machines: list[Machine]) -> int:
+    """Solves part 2, this also works for part one and is faster."""
     result = 0
     for machine in machines:
-        result += eucledes(machine)
+        result += solveOne(machine)
     return result
 
 
 if __name__ == '__main__':
     inp = format_input(get_input('day13.txt'))
-    #res = solve(inp)
-    #print(res)
+    res = solve(inp)
+    print(res)
+    # This line is just adds 10000000000000 to all input Machines, it is very readable thank you.
     res = solve2(list(map((lambda mach: [mach[0], mach[1], (mach[2][0] + 10000000000000, mach[2][1] + 10000000000000)]), inp)))
     print(res)
