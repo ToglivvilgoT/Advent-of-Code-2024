8fd56a852e0d818668d3678153f792172b42a875
ToglivvilgoT <vilgot.johansen@hotmail.com>
2024-12-16 13:35:35 +0100

    cleanup day 12

--- day12.py
+++ day12.py
@@ -1,9 +1,12 @@
-from aoc import get_input, get_input_lines, uncurry, in_bounds, get_neighbours
-from functools import partial, reduce
-from itertools import product
+from aoc import get_input, in_bounds, get_neighbours
 
 
-def flood_fill(map: list[str], x, y):
+Position = tuple[int, int]
+Region = set[Position]
+
+
+def flood_fill(map: list[str], x: int, y: int) -> Region:
+    """Extract the region connected to x and y using flood fill."""
     region = set()
     typ = map[y][x]
     stack = [(x, y)]
@@ -21,7 +24,8 @@ def flood_fill(map: list[str], x, y):
     return region
 
 
-def format_input(inp: str):
+def format_input(inp: str) -> list[Region]:
+    """Formats the input."""
     visited = set()
     regions = []
     lines = inp.splitlines()
@@ -35,7 +39,8 @@ def format_input(inp: str):
     return regions
 
 
-def get_perimiter(region):
+def get_perimiter(region: Region) -> int:
+    """Returns the perimiter of a region."""
     perimiter = 0
     for tile in region:
         fences = 4
@@ -46,7 +51,8 @@ def get_perimiter(region):
     return perimiter
 
 
-def solve(regions):
+def solve(regions: list[Region]) -> int:
+    """Solves part 1 and returns the answer."""
     answer = 0
     for region in regions:
         area = len(region)
@@ -55,29 +61,45 @@ def solve(regions):
     return answer
 
 
-def get_neighbours2(pos):
+def get_neighbours2(pos: Position) -> list[tuple[Position, Position, Position]]:
+    """Given a position, return a list of 4 tuples,
+    each tuple has 3 positions coresponding to one of the four corners
+    of the 3x3 square where pos is the middle tile.
+    """
+    RIGHT = (1, 0)
+    UP_RIGHT = (1, -1)
+    UP = (0, -1)
+    UP_LEFT = (-1, -1)
+    LEFT = (-1, 0)
+    DOWN_LEFT = (-1, 1)
+    DOWN = (0, 1)
+    DOWN_RIGHT = (1, 1)
+
     x, y = pos
     neighbours = []
     for (dx, dy), (dx2, dy2), (dx3, dy3) in [
-            ((0, -1), (-1, 0), (-1, -1)),
-            ((1, 0), (0, -1), (1, -1)),
-            ((0, 1), (1, 0), (1, 1)),
-            ((-1, 0), (0, 1), (-1, 1)),
+            (UP, LEFT, UP_LEFT),
+            (RIGHT, UP, UP_RIGHT),
+            (DOWN, RIGHT, DOWN_RIGHT),
+            (LEFT, DOWN, DOWN_LEFT),
     ]:
         neighbours.append(((x + dx, y + dy), (x + dx2, y + dy2), (x + dx3, y + dy3)))
     return neighbours
 
 
-def get_sides(region):
+def get_sides(region: Region) -> int:
+    """Return amount of sides of region."""
     sides = 0
     for tile in region:
         for neighbour, neighbour2, neighbour3 in get_neighbours2(tile):
+            # This is logic, it works so don't tuch it.
             if not (neighbour in region or neighbour2 in region and neighbour3 not in region):
                 sides += 1
     return sides
 
 
-def solve2(regions):
+def solve2(regions: list[Region]) -> int:
+    """Solves part 2 and returns the answer."""
     answer = 0
     for region in regions:
         area = len(region)
