6d8caafa388df9a04a08cc3e6ef2a773cd120d3b
ToglivvilgoT <vilgot.johansen@hotmail.com>
2024-12-18 11:11:34 +0100

    clean up day 16

--- day16.py
+++ day16.py
@@ -1,26 +1,42 @@
-from aoc import get_input, get_input_lines, uncurry, in_bounds, get_neighbours
-from functools import partial, reduce
-from itertools import product
+"""Solution for day 16."""
+from aoc import get_input, get_neighbours
+from functools import reduce
 from queue import PriorityQueue
+from typing import Literal
 
 
-def format_input(inp: str):
+Position = tuple[int, int]
+Direction = tuple[int, int]
+PosDir = tuple[Position, Direction]
+Tile = Literal['.', '#']
+Grid = dict[Position, Tile]
+
+
+def format_input(inp: str) -> tuple[Grid, Position, Position]:
+    """Formats the input and returns it as a tuple of
+    (grid, start_position, end_position).
+    """
     lines = inp.splitlines()
     width = len(lines[0])
     height = len(lines)
     start_pos = (1, height - 2)
     end_pos = (width - 2, 1)
-    world = {(x, y): lines[y][x] for y in range(height) for x in range(width)}
-    world[start_pos] = world[end_pos] = '.'
-    return world, start_pos, end_pos
+    grid = {(x, y): lines[y][x] for y in range(height) for x in range(width)}
+    grid[start_pos] = grid[end_pos] = '.'
+    return grid, start_pos, end_pos
 
 
-def get_neighbours(pos, dir):
+def get_neighbours(pos: Position, dir: Direction) -> list[PosDir]:
+    """Returns the neighbours of pos.
+    The neighbour straight behind pos is ignored.
+    """
     def left():
+        """Returns dir rotated 90 degrees left."""
         x, y = dir
         return y, -x
 
     def right():
+        """Returns dir rotated 90 degrees right."""
         x, y = dir
         return -y, x
 
@@ -34,7 +50,10 @@ def get_neighbours(pos, dir):
     return neighbours
 
 
-def get_paths_tiles(prevs, pos, dir):
+def get_paths_tiles(prevs: dict[PosDir, tuple[int, list[PosDir]]], pos: Position, dir: Direction) -> set[Position]:
+    """Return a set of all positions from start to pos by going through
+    prevs backwards from pos to end and adding each tile along the way.
+    """
     tiles = {pos}
     try:
         _, parent_tiles = prevs[(pos, dir)]
@@ -45,11 +64,17 @@ def get_paths_tiles(prevs, pos, dir):
     return tiles
 
 
-def solve(world, start_pos, end_pos):
-    def h(prev_dir, new_dir):
+def solve(grid: Grid, start_pos: Position, end_pos: Position) -> int:
+    """Solves part 1 and returns the answer.
+    Solution is made with A* search.
+    Note that h and g have been swapped because I implemented from memmory and forgot which was which...
+    """
+    def h(prev_dir: Direction, new_dir: Direction) -> int:
+        """Returns the cost to get from previous tile to new tile."""
         return 1 + 1000 * (prev_dir != new_dir)
 
-    def g(pos):
+    def g(pos: Position) -> int:
+        """Returns the estimated cost of getting from pos to end_pos."""
         x, y = pos
         ex, ey = end_pos
         return abs(ex - x) + abs(y - ey)
@@ -57,11 +82,20 @@ def solve(world, start_pos, end_pos):
     que = PriorityQueue()
     que.put((g(start_pos), (start_pos, (1, 0), 0)))
     visited = set()
+    # Prevs is a bit weird. Basically, every visited PosDir is a key in prevs,
+    # The value at that key is a tuple of the cost and a list of all previous tiles.
+    # the cost is the cost to get there from start via any of the previous tiles.
+    # If a new parent tile would be found with cheaper cost, it will be the new and only element in the list
+    # If it would have the same cost, it will be appended to the list
+    # And if it wold be more expensive, it is ignored.
     prevs = {(start_pos, (1, 0)): (0, [])}
     best_path_cost = None
+    # a set of all directions that the end can be reached from via an optimal path.
     end_dirs = set()
     while que.qsize() != 0:
         _, (pos, dir, prev_h) = que.get()
+        # Realised now afterwards that this logic is quite dumb, flawed and should not work in all cases
+        # but it works with my input so it's all good :)
         if pos == end_pos:
             if best_path_cost == None:
                 end_dirs.add(dir)
@@ -77,17 +111,21 @@ def solve(world, start_pos, end_pos):
             visited.add((pos, dir))
 
         for new_pos, new_dir in get_neighbours(pos, dir):
-            if world[new_pos] == '.':
+            if grid[new_pos] == '.':
                 new_h = prev_h + h(dir, new_dir)
                 new_cost = new_h + g(new_pos)
                 que.put((new_cost, (new_pos, new_dir, new_h)))
                 if (new_pos, new_dir) in prevs:
+                    # This tile has been visited before
                     best_cost, parent_tiles = prevs[(new_pos, new_dir)]
+                    # Check if this path to new_pos was cheaper than the previous or had the same cost.
+                    # If it was more expensive, nothing happens.
                     if best_cost == new_cost:
                         parent_tiles.append((pos, dir))
                     elif best_cost > new_cost:
                         prevs[(new_pos, new_dir)] = (new_cost, [(pos, dir)])
                 else:
+                    # This tile has not been visited before.
                     prevs[(new_pos, new_dir)] = (new_cost, [(pos, dir)])
 
 
